9475bd9026b380090043fc80ba282654d9320ccc
nan21eu <nan21eu@d74032a6-9db0-11dd-bde8-492b52a95b86>
2008-12-02 12:52:55 +0000

    v0.5

---
+++ trunk/product/src/control/DC0071.php
@@ -0,0 +1,291 @@
+<?php
+/* N21 eBusiness Suite
+ * Copyright: Nan21 Electronics srl
+ * Generated content.
+ * DC0071 Controller: UI_DC_ROLE_PERMISSION
+ */
+
+require_once(PATH_CTRL_FRMWK."/Controller.php");
+
+class DC0071 extends Controller {
+
+
+private function preQuery(&$params, &$where) {
+    if (!empty($_REQUEST["QRY_DELETE_ALLOWED"])) {
+      $where .= (!empty($where))?" and ":"";
+      $where .= "DELETE_ALLOWED like :DELETE_ALLOWED";
+      $params["DELETE_ALLOWED"] = $_REQUEST["QRY_DELETE_ALLOWED"];
+    }
+    if (!empty($_REQUEST["QRY_FETCH_ALLOWED"])) {
+      $where .= (!empty($where))?" and ":"";
+      $where .= "FETCH_ALLOWED like :FETCH_ALLOWED";
+      $params["FETCH_ALLOWED"] = $_REQUEST["QRY_FETCH_ALLOWED"];
+    }
+    if (!empty($_REQUEST["QRY_ID"])) {
+      $where .= (!empty($where))?" and ":"";
+      $where .= "ID like :ID";
+      $params["ID"] = $_REQUEST["QRY_ID"];
+    }
+    if (!empty($_REQUEST["QRY_INSERT_ALLOWED"])) {
+      $where .= (!empty($where))?" and ":"";
+      $where .= "INSERT_ALLOWED like :INSERT_ALLOWED";
+      $params["INSERT_ALLOWED"] = $_REQUEST["QRY_INSERT_ALLOWED"];
+    }
+    if (!empty($_REQUEST["QRY_ROLE_NAME"])) {
+      $where .= (!empty($where))?" and ":"";
+      $where .= "ROLE_NAME like :ROLE_NAME";
+      $params["ROLE_NAME"] = $_REQUEST["QRY_ROLE_NAME"];
+    }
+    if (!empty($_REQUEST["QRY_UI_DC"])) {
+      $where .= (!empty($where))?" and ":"";
+      $where .= "UI_DC like :UI_DC";
+      $params["UI_DC"] = $_REQUEST["QRY_UI_DC"];
+    }
+    if (!empty($_REQUEST["QRY_UPDATE_ALLOWED"])) {
+      $where .= (!empty($where))?" and ":"";
+      $where .= "UPDATE_ALLOWED like :UPDATE_ALLOWED";
+      $params["UPDATE_ALLOWED"] = $_REQUEST["QRY_UPDATE_ALLOWED"];
+    }
+}
+
+public function doQuery() {
+  try {
+    $start = nvl($this->getRequestParam("start"), 0);
+    $limit = nvl($this->getRequestParam("limit"),20);
+    $orderBy = (!empty($_REQUEST["sort"]))?$_REQUEST["sort"]:"";
+    $orderSense = (!empty($_REQUEST["dir"]))?$_REQUEST["dir"]:"";
+    $orderByClause = (!empty($orderBy))? "order by $orderBy $orderSense " : "" ;
+    $where = "";
+    $params = array();
+    $this->preQuery($params, $where);
+    if (!empty($where)) {
+      $where = " where ".$where;
+    }
+    $sql = "select 
+                CREATEDBY
+                ,CREATEDON
+                ,DELETE_ALLOWED
+                ,FETCH_ALLOWED
+                ,ID
+                ,INSERT_ALLOWED
+                ,MODIFIEDBY
+                ,MODIFIEDON
+                ,ROLE_NAME
+                ,UI_DC
+                ,UPDATE_ALLOWED
+            from UI_DC_ROLE_PERMISSION  $where $orderByClause ";
+    $rs = $this->db->SelectLimit($sql, $limit, $start, $params);
+    $rsCount = $this->db->Execute("select count(*) TOTALCOUNT from (".$sql.") t", $params);
+    $rsCount->MoveFirst();
+    $columns = array(
+      "CREATEDBY"
+      ,"CREATEDON"
+      ,"DELETE_ALLOWED"
+      ,"FETCH_ALLOWED"
+      ,"ID"
+      ,"INSERT_ALLOWED"
+      ,"MODIFIEDBY"
+      ,"MODIFIEDON"
+      ,"ROLE_NAME"
+      ,"UI_DC"
+      ,"UPDATE_ALLOWED"
+      );
+    $dataOut = $this->serializeCursor($rs,$columns, $this->query_data_format);
+    if ($this->query_data_format == "xml" ) {header("Content-type: application/xml");}
+    print $this->formatQueryResponseData($dataOut, $rsCount->fields["TOTALCOUNT"]);
+  }catch(Exception  $e) {
+    System::sendActionErrorJson( $e->getMessage());
+  }
+} /* end function doQuery  */
+
+
+public function doExport() {
+  try {
+    $start = nvl($this->getRequestParam("start"), 0);
+    $limit = nvl($this->getRequestParam("limit"),20);
+    $groupBy = (!empty($_REQUEST["groupBy"]))?$_REQUEST["groupBy"]:"";
+    $orderBy = (!empty($_REQUEST["sort"]))?$_REQUEST["sort"]:"";
+    $orderSense = (!empty($_REQUEST["dir"]))?$_REQUEST["dir"]:"";
+    $orderByClause = (!empty($orderBy))? "order by $orderBy $orderSense " : "" ;
+    $where = "";
+    $params = array();
+    $this->preQuery($params, $where);
+    if (!empty($where)) {
+      $where = " where ".$where;
+    }
+    $sql = "select 
+                ID
+                ,UI_DC
+                ,ROLE_NAME
+                ,FETCH_ALLOWED
+                ,INSERT_ALLOWED
+                ,UPDATE_ALLOWED
+                ,DELETE_ALLOWED
+                ,CREATEDON
+                ,CREATEDBY
+                ,MODIFIEDON
+                ,MODIFIEDBY
+            from UI_DC_ROLE_PERMISSION  $where $orderByClause ";
+    $rs = $this->db->Execute($sql, $params);
+    $rsCount = $this->db->Execute("select count(*) TOTALCOUNT from (".$sql.") t", $params);
+    $rsCount->MoveFirst();
+    $columns = array(
+     "ID"
+     ,"UI_DC"
+     ,"ROLE_NAME"
+     ,"FETCH_ALLOWED"
+     ,"INSERT_ALLOWED"
+     ,"UPDATE_ALLOWED"
+     ,"DELETE_ALLOWED"
+     ,"CREATEDON"
+     ,"CREATEDBY"
+     ,"MODIFIEDON"
+     ,"MODIFIEDBY"
+      );
+    if (!empty($_REQUEST["_p_disp_cols"])) {
+      $columns = explode("|",$_REQUEST["_p_disp_cols"]);
+    }
+    $dataOut = $this->serializeCursor($rs,$columns,"xml");
+    $dataOut = "<records>".$dataOut."</records>";
+    $dataOut = "<queryParams>".$this->serializeArray($params,"xml")."</queryParams>".$dataOut;
+    $dataOut = "<columnDef>".$this->columnDefForExport($columns,$this->fieldDef,true).$this->columnDefForExport(array_diff(array_keys($params), $columns),$this->fieldDef,false)."</columnDef>".$dataOut;
+    $dataOut = "<staticText>".$this->exportLocalizedStaticText()."</staticText>".$dataOut;
+    $dataOut = "<groupBy>".$groupBy."</groupBy>".$dataOut;
+    $dataOut = "<reportData  title=\"".$this->getDcTitle()."\" by=\"".$_SESSION["user"]["userName"]."\" on=\"".date(DATE_FORMAT)."\">".$dataOut."</reportData>";
+    $this->beginExport();
+    print $dataOut;
+    $this->endExport();
+  }catch(Exception  $e) {
+    System::sendActionErrorJson( $e->getMessage());
+  }
+} /* end function doExport  */
+
+
+public function fetchRecord() {
+  try {
+    $RECORD = array();
+    $RECORD["ID"] = $this->getRequestParam("ID");
+    if (empty($RECORD["ID"])) { throw new Exception("Missing value for primary key field ID in DC0071.fetchRecord().");}
+    $pkArray = array("ID" => $RECORD["ID"]);
+    $this->findByPk($pkArray, $RECORD);
+    print "{success:true, data:".json_encode($RECORD)."}";
+  }catch(Exception  $e) {
+    System::sendActionErrorJson( $e->getMessage());
+  }
+} /* end function fetchRecord */
+
+
+public function doUpdate() {
+  try {
+    $RECORD["_p_record_status"] = $this->getRequestParam("_p_record_status");
+    $RECORD["_p_store_recId"] = $this->getRequestParam("_p_store_recId");
+    $RECORD["DELETE_ALLOWED"] = $this->getRequestParam("DELETE_ALLOWED");
+    $RECORD["FETCH_ALLOWED"] = $this->getRequestParam("FETCH_ALLOWED");
+    $RECORD["ID"] = $this->getRequestParam("ID");
+    $RECORD["INSERT_ALLOWED"] = $this->getRequestParam("INSERT_ALLOWED");
+    $RECORD["UPDATE_ALLOWED"] = $this->getRequestParam("UPDATE_ALLOWED");
+    $sql = "update UI_DC_ROLE_PERMISSION set 
+                 DELETE_ALLOWED=:DELETE_ALLOWED
+                ,FETCH_ALLOWED=:FETCH_ALLOWED
+                ,ID=:ID
+                ,INSERT_ALLOWED=:INSERT_ALLOWED
+                ,UPDATE_ALLOWED=:UPDATE_ALLOWED
+    where 
+           ID= :ID
+    ";
+    $stmt = $this->db->prepare($sql);
+    $this->db->Execute($stmt, $RECORD);
+    print "{success:true, data:".json_encode($RECORD)."}";
+  }catch(Exception  $e) {
+    System::sendActionErrorJson( $e->getMessage());
+  }
+} /* end function doUpdate */
+
+
+public function initNewRecord() {
+  try {
+    $RECORD["CREATEDBY"] = $this->getRequestParam("CREATEDBY");
+    $RECORD["CREATEDON"] = $this->getRequestParam("CREATEDON");
+    $RECORD["DELETE_ALLOWED"] = $this->getRequestParam("DELETE_ALLOWED");
+    $RECORD["FETCH_ALLOWED"] = $this->getRequestParam("FETCH_ALLOWED");
+    $RECORD["ID"] = $this->getRequestParam("ID");
+    $RECORD["INSERT_ALLOWED"] = $this->getRequestParam("INSERT_ALLOWED");
+    $RECORD["MODIFIEDBY"] = $this->getRequestParam("MODIFIEDBY");
+    $RECORD["MODIFIEDON"] = $this->getRequestParam("MODIFIEDON");
+    $RECORD["ROLE_NAME"] = $this->getRequestParam("ROLE_NAME");
+    $RECORD["UI_DC"] = $this->getRequestParam("UI_DC");
+    $RECORD["UPDATE_ALLOWED"] = $this->getRequestParam("UPDATE_ALLOWED");
+    $RECORD["_p_record_status"] = "insert";
+    $this->setFieldInitialValues($RECORD, "DC0071");
+    print "{success:true, data:".json_encode($RECORD)."}";
+  }catch(Exception  $e) {
+    System::sendActionErrorJson( $e->getMessage());
+  }
+} /* end function initNewRecord */
+
+private function findByPk(&$pkCols, &$record) {
+    $sql = "select 
+                CREATEDBY
+                ,CREATEDON
+                ,DELETE_ALLOWED
+                ,FETCH_ALLOWED
+                ,ID
+                ,INSERT_ALLOWED
+                ,MODIFIEDBY
+                ,MODIFIEDON
+                ,ROLE_NAME
+                ,UI_DC
+                ,UPDATE_ALLOWED
+            from UI_DC_ROLE_PERMISSION 
+         where 
+           ID= :ID
+            ";
+    $rs = $this->db->Execute($sql, $pkCols);
+    $row = $rs->FetchRow();
+    $this->copyArrayValues($record, $row);
+} /* end function findByPk  */
+
+private  $fieldDef = array(
+  "CREATEDBY" => array("DATA_TYPE" => "STRING")
+  ,"CREATEDON" => array("DATA_TYPE" => "DATE")
+  ,"DELETE_ALLOWED" => array("DATA_TYPE" => "BOOLEAN")
+  ,"FETCH_ALLOWED" => array("DATA_TYPE" => "BOOLEAN")
+  ,"ID" => array("DATA_TYPE" => "NUMBER")
+  ,"INSERT_ALLOWED" => array("DATA_TYPE" => "BOOLEAN")
+  ,"MODIFIEDBY" => array("DATA_TYPE" => "STRING")
+  ,"MODIFIEDON" => array("DATA_TYPE" => "DATE")
+  ,"ROLE_NAME" => array("DATA_TYPE" => "STRING")
+  ,"UI_DC" => array("DATA_TYPE" => "STRING")
+  ,"UPDATE_ALLOWED" => array("DATA_TYPE" => "BOOLEAN")
+);
+
+
+private function readRequest(&$RECORD) {
+     if (isset($_REQUEST["CREATEDBY"] )) { $RECORD["CREATEDBY"] = $this->getRequestParam("CREATEDBY"); }
+     if (isset($_REQUEST["CREATEDON"] )) { $RECORD["CREATEDON"] = $this->getRequestParam("CREATEDON"); }
+    $RECORD["DELETE_ALLOWED"] = $this->getRequestParamBoolean("DELETE_ALLOWED");
+    $RECORD["FETCH_ALLOWED"] = $this->getRequestParamBoolean("FETCH_ALLOWED");
+     if (isset($_REQUEST["ID"] )) { $RECORD["ID"] = $this->getRequestParam("ID"); }
+    $RECORD["INSERT_ALLOWED"] = $this->getRequestParamBoolean("INSERT_ALLOWED");
+     if (isset($_REQUEST["MODIFIEDBY"] )) { $RECORD["MODIFIEDBY"] = $this->getRequestParam("MODIFIEDBY"); }
+     if (isset($_REQUEST["MODIFIEDON"] )) { $RECORD["MODIFIEDON"] = $this->getRequestParam("MODIFIEDON"); }
+     if (isset($_REQUEST["ROLE_NAME"] )) { $RECORD["ROLE_NAME"] = $this->getRequestParam("ROLE_NAME"); }
+     if (isset($_REQUEST["UI_DC"] )) { $RECORD["UI_DC"] = $this->getRequestParam("UI_DC"); }
+    $RECORD["UPDATE_ALLOWED"] = $this->getRequestParamBoolean("UPDATE_ALLOWED");
+} /* end function readRequest  */
+
+
+public function callProcedure($pName) {
+  $this->logger->debug("Start: ".$this->dcName.".callProcedure(".$pName.")");
+  try {
+    $RECORD = array();
+    $this->readRequest($RECORD);
+    print "{success:true, data:".json_encode($RECORD)."}";
+    $this->logger->debug("End: ".$this->dcName.".callProcedure");
+  }catch(Exception  $e) {
+      System::sendActionErrorJson( $e->getMessage());
+  }
+} /* end function callProcedure */
+
+}
+?>
