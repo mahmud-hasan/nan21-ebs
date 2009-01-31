f8dca1b47f48f73ef06e1046e55bb6cf59a299f2
nan21eu <nan21eu@d74032a6-9db0-11dd-bde8-492b52a95b86>
2009-01-31 09:44:19 +0000

    v 0.7

--- trunk/product/src/control/LOV0043.php
+++ trunk/product/src/control/LOV0043.php
@@ -9,13 +9,21 @@ class LOV0043 extends Controller {
   public function doQuery() {
     try {
       $params = array();
+      $p_query_column = $this->getRequestParam("_p_query_column");
+      $p_query_value = $this->getRequestParam("_p_query_value");
+      $where = "";
+      if (!empty($p_query_value )) {
+        if (!empty($where)) {
+          $where = " where ".$where;
+          $params ["p_query_value"] = $p_query_value;
+        }
+      }
       $orderBy = (!empty($_REQUEST["sort"]))?$_REQUEST["sort"]:"";
       $orderSense = (!empty($_REQUEST["dir"]))?$_REQUEST["dir"]:"";
       $orderByClause = (!empty($orderBy))? "order by $orderBy $orderSense " : "" ;
       $params["p_client_id"] = (isset($_REQUEST["p_client_id"]))?  $_REQUEST["p_client_id"] : "-1";
-      $sql = "select id, name 
-  from acc_schema 
- where client_id = :p_client_id ".$orderByClause;
+      $sql = "select tsql.* from (select id, name 
+  from ac_accschema) tsql ".$where." ".$orderByClause;
       $stmt = $this->db->prepare($sql);
       $rs = $this->db->Execute($stmt, $params);
       $columns = array(
