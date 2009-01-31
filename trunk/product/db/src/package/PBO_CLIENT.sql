b97cd0e0e8be084e0b31098606dbee7664eb289a
nan21eu <nan21eu@d74032a6-9db0-11dd-bde8-492b52a95b86>
2009-01-31 07:21:51 +0000

    v 0.7

--- trunk/product/db/src/package/PBO_CLIENT.sql
+++ trunk/product/db/src/package/PBO_CLIENT.sql
@@ -1,7 +1,7 @@
 create or replace package pbo_client is
 
-  function get_code_by_id(p_client_id in number, p_raise in char default 'N') return varchar2;
-  function get_name_by_id(p_client_id in number, p_raise in char default 'N') return varchar2;
+  function get_code_by_id(p_id in number, p_raise in char default 'N') return varchar2;
+  function get_name_by_id(p_id in number, p_raise in char default 'N') return varchar2;
 end;
 /
 show errors package PBO_CLIENT; 
@@ -11,51 +11,26 @@ show errors package PBO_CLIENT;
 create or replace package body pbo_client is
 
   /* Get client code by its ID
-   * Params: p_client_id: Client ID
+   * Params: p_id: Client ID
    *         p_raise: Raise error if client with given id is not found (Y=yes, N=no)
    */
-  function get_code_by_id(p_client_id in number, p_raise in char default 'N') return varchar2 is 
-    cursor c is 
-      select a.code
-        from client a
-       where a.id = p_client_id;  
-    r c%rowtype; 
-    b boolean;  
-  begin
-    open c;
-    fetch c into r;
-    b := c%found;
-    close c;
-    if p_raise = 'Y' and not b then 
-      raise_error('ClientByIdNotFound');
-    end if; 
-    return r.code;    
+  function get_code_by_id(p_id in number, p_raise in char default 'N') return varchar2 is 
+    vrec client%rowtype;
+  begin     
+    pdao_findby_pk.client_t(p_id, vrec, p_raise);
+    return vrec.code;        
   end;    
   
   
-  
-  
-  
   /* Get client name by its ID
-   * Params: p_client_id: Client ID
+   * Params: p_id: Client ID
    *         p_raise: Raise error if client with given id is not found (Y=yes, N=no)
    */
-  function get_name_by_id(p_client_id in number, p_raise in char default 'N') return varchar2 is 
-    cursor c is 
-      select a.name
-        from client a
-       where a.id = p_client_id;  
-    r c%rowtype; 
-    b boolean;  
-  begin
-    open c;
-    fetch c into r;
-    b := c%found;
-    close c;
-    if p_raise = 'Y' and not b then 
-      raise_error('ClientByIdNotFound');
-    end if; 
-    return r.name;    
+  function get_name_by_id(p_id in number, p_raise in char default 'N') return varchar2 is 
+    vrec client%rowtype;
+  begin     
+    pdao_findby_pk.client_t(p_id, vrec, p_raise);
+    return vrec.name; 
   end;  
   
 end;
