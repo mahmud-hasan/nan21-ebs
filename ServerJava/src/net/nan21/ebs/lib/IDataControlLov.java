91526cb9610097978b0294e1fb612f50f00e3e80
nan21eu <nan21eu@d74032a6-9db0-11dd-bde8-492b52a95b86>
2009-05-09 08:09:52 +0000

    v0.9

--- ServerJava/src/net/nan21/ebs/lib/IDataControlLov.java
+++
@@ -1,8 +0,0 @@
-package net.nan21.ebs.lib;
-
-import javax.servlet.http.HttpServletResponse;
-
-public interface IDataControlLov {
-	public void init(HttpRequest request, HttpServletResponse response, HttpSession session, DbManager dbm) throws Exception;
-	public void doQuery()  throws Exception ;
-}
