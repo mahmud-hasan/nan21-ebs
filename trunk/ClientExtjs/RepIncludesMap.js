09c22d626c70dfabc1e887255b56aa03102f7159
nan21eu <nan21eu@d74032a6-9db0-11dd-bde8-492b52a95b86>
2009-05-28 13:01:43 +0000

    v0.9

---
+++ trunk/ClientExtjs/RepIncludesMap.js
@@ -0,0 +1,9 @@
+/* 
+ * Define includes map for Report components.
+ * Copyright: Nan21 Electronics srl
+ * Generated content.
+ */
+function initRepIncludes(m) {
+  m.add("REP0002",["LOV0001","LOV0003","LOV0008","LOV0009"]);
+  m.add("REP0003",["LOV0067"]);
+}
